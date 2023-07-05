b69548212e1ceba40b70a50ec9b0a83529b2e188
zhangqiquan <768617998@qq.com>
2023-07-05 15:28:22 +0800

    jsonRpc 连接维持和失败重试一次

--- src/zhanshop/client/JsonRpcClient.php
+++ src/zhanshop/client/JsonRpcClient.php
@@ -22,6 +22,49 @@ class JsonRpcClient
     protected $timeout = 3;
 
     /**
+     * 已建立的连接
+     * @var Client
+     */
+    protected static array $clients = [];
+
+    /**
+     * 连接
+     * @param string $host
+     * @param int $port
+     * @param float $timeout
+     * @return void
+     */
+    public static function client(string $host, int $port = 6204, float $timeout = 3){
+        $client = self::$clients[$host.$port] ?? null;
+
+        if($client && $client->isConnected() != false){
+            return $client;
+        }
+
+        $client = new Client(SWOOLE_SOCK_TCP);
+        if (!$client->connect($host, $port, $timeout)){
+            App::error()->setError($host.':'.$port.'连接失败#'.$client->errCode);
+        }
+        self::$clients[$host.$port] = $client;
+
+        return $client;
+    }
+
+    /**
+     * 销毁连接
+     * @param string $host
+     * @param int $port
+     * @return void
+     */
+    public static function close(string $host, int $port = 6204){
+        if(isset(self::$clients[$host.$port])){
+            $client = self::$clients[$host.$port];
+            $client->close();
+            unset(self::$clients[$host.$port]);
+        }
+    }
+
+    /**
      * 设置grpc服务地址
      * @param string $name
      * @return $this
@@ -40,19 +83,24 @@ class JsonRpcClient
      * @return mixed
      * @throws \Exception
      */
-    public function request(string $path, mixed $data){
-        $client = new Client(SWOOLE_SOCK_TCP);
-        if (!$client->connect($this->serverHost, $this->serverPort, $this->timeout)){
-            App::error()->setError($this->serverHost.':'.$this->serverPort.'连接失败#'.$client->errCode);
-        }
+    public function request(string $path, mixed $data, bool $again = false){
+        $client = JsonRpcClient::client($this->serverHost, $this->serverPort, $this->timeout);
         $requestData = [
             'path' => $path,
             'header' => [],
             'body' => $data,
         ];
-
         $client->send(json_encode($requestData, JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE)."\r\n");
         $resp =  $client->recv();
+        
+        if($resp == false){
+            JsonRpcClient::close($this->serverHost, $this->serverPort);
+            if($again == false){
+                return $this->request($path, $data, true); // 再试一次
+            }
+            App::error()->setError($this->serverHost.':'.$this->serverPort.$path.' jsonRpc请求失败：'.$resp);
+        }
+        
         $data = json_decode($resp, true);
         if($data['code'] != 0){
             App::error()->setError($this->serverHost.':'.$this->serverPort.$path.' jsonRpc请求错误：'.$resp, $data['code']);
