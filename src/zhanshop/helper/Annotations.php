3ad1d437278a79f6f210f558f7dbe630ea56e376
张启全 <admin@zhanshop.cn>
2023-08-26 11:25:57 +0800

    ()当做一个整体 对空号内匹配到的数据单独放一个下标

--- src/zhanshop/helper/Annotations.php
+++ src/zhanshop/helper/Annotations.php
@@ -16,6 +16,7 @@ class Annotations
         // @Route(articleList/{id}, GET) 可能没有空格
         // \s* 零个或多个空白字符
         // ^\s 以空白字符开头
+        //()当做一个整体 对空号内匹配到的数据单独放一个下标
         $matched = preg_match('/@'.$name.'(\s*.*)/i', $note, $matches);
         var_dump($matches);
     }
