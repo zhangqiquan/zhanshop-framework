88698e8c7acb37c60eb0806ca10c2d06404c10c9
张启全 <admin@zhanshop.cn>
2023-08-27 15:30:57 +0800

    update

--- src/zhanshop/helper/Annotations.php
+++ src/zhanshop/helper/Annotations.php
@@ -1,17 +1,69 @@
 <?php
 
-namespace zhanshop\service;
+namespace zhanshop\helper;
 /**
  * 注解支持123
  * @api {method} path title
  * @apiGroup User
  * @apiHeader {String} 字段名 描述
  * @apiParam {Int} page=1 页码
+ * @apiSuccess string 字段名 描述
+ * @apiError CODE 错误说明
  * @apiMiddleware 1,2
  * @apiDescription text
  */
 class Annotations
 {
+    protected $docComment;
+    public function __construct(string $docComment)
+    {
+        $this->docComment = $docComment;
+    }
+
+    public function api(){
+        $matched = preg_match('/@api\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiGroup(){
+        $matched = preg_match('/@apiGroup\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiMiddleware(){
+        $matched = preg_match('/@apiMiddleware\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiHeader(){
+        $matched = preg_match('/@apiHeader\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiParam(){
+        $matched = preg_match_all('/@apiParam\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiSuccess(){
+        $matched = preg_match_all('/@apiSuccess\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function apiError(){
+        $matched = preg_match_all('/@apiError\s*(.*)/i', $this->docComment, $matches);
+        var_dump($matches);
+    }
+
+    public function all(){
+        $this->api();
+        $this->apiGroup();
+        $this->apiMiddleware();
+        $this->apiHeader();
+        $this->apiParam();
+        $this->apiSuccess();
+        $this->apiError();
+    }
     public static function getTitle(string $note){
         $note = explode("\n", $note)[1];
         $matched = preg_match('/\* (.*)/i', $note, $matches);
