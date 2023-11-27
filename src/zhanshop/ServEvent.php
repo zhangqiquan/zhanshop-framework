229dacaf0d218617aeee4511e7e3de6c7c594014
张启全 <admin@zhanshop.cn>
2023-11-27 15:04:32 +0800

    update

--- src/zhanshop/ServEvent.php
+++ src/zhanshop/ServEvent.php
@@ -232,7 +232,7 @@ class ServEvent
      * @param \Swoole\Http\Request $request
      * @return void
      */
-    public function onOpen(\Swoole\WebSocket\Server $server, mixed $request) :void{
+    public function onOpen(\Swoole\WebSocket\Server $server, \Swoole\Http\Request $request) :void{
     }
 
     /**
